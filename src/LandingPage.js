c3024c8fde188246db0e66485c105336868bc1f6
DevPeter <polalekan526@gmail.com>
2023-09-21 18:03:54 +0100

    Landing Page Created

---
+++ src/LandingPage.js
@@ -0,0 +1,23 @@
+const Landing = () => {
+    return ( 
+        <>
+            <section>
+                <section className="grid grid-cols-2 gap-3 mt-20 px-32">
+                        <div >
+                                <p className="text-7xl font-bold">
+                                    Get Access To Data
+                                </p>
+                                <div className="bg-blue-500 text-white py-2 px-4 w-32 text-center mt-4 rounded-md">
+                                    Buy now
+                                </div>
+                        </div>
+                        <div>
+                                5
+                        </div>
+                </section>
+            </section>
+        </>
+     );
+}
+ 
+export default Landing;
