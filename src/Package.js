93ce4dba220afdd71199239082bc4377ae5581b0
DevPeter <polalekan526@gmail.com>
2023-09-25 12:49:50 +0100

    Data package added

--- src/Package.js
+++ src/Package.js
@@ -5,13 +5,54 @@ const Package = () => {
                     <h5 className="text-6xl text-center my-16">Our Data Package</h5>    
 
                     <div>
-                        <div className="plan">
-                                    <div>
-                                        <div>
-                                            <div className="py-4 border-2 border-blue-400">MTN</div>
+                        <div className="allPlan mt-44">
+                                    <div className="plan">
+                                    <div className="grid grid-cols-3 gap-20">
 
+                                        <div className="w-44">
+                                            <div className="py-4 shadow-lg  border-2 border-blue-400 text-center rounded-lg w-40 mx-6 mfont-bold">MTN</div>
                                             
+                                        <div className="bg-white mt-4 p-10 w-52 text-center shadow-lg rounded-lg">
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+
+                                            <button className="bg-blue-500 py-2 px-6 rounded-lg mt-4 font-bold text-white">Buy Now</button>
                                         </div>
+                                        </div>
+
+
+                                        <div className="w-44">
+                                            <div className="py-4 shadow-lg  border-2 border-blue-400 text-center rounded-lg w-40 mx-6 mfont-bold">MTN</div>
+                                            
+                                        <div className="bg-white mt-4 p-10 w-52 text-center shadow-lg rounded-lg">
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+
+                                            <button className="bg-blue-500 py-2 px-6 rounded-lg mt-4 font-bold text-white">Buy Now</button>
+                                        </div>
+                                        </div>
+
+                                         <div className="w-44">
+                                            <div className="py-4 shadow-lg  border-2 border-blue-400 text-center rounded-lg w-40 mx-6 mfont-bold">MTN</div>
+                                            
+                                        <div className="bg-white mt-4 p-10 w-52 text-center shadow-lg rounded-lg">
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+
+                                            <button className="bg-blue-500 py-2 px-6 rounded-lg mt-4 font-bold text-white">Buy Now</button>
+                                        </div>
+
+                                        
+                                        
+                                        </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div className="pt-96">
+                                            odogwu
                                     </div>
                         </div>
                     </div>
