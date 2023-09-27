86ad3a768627736d366625f2b95b187a94427e8d
DevPeter <polalekan526@gmail.com>
2023-09-27 14:41:50 +0100

    Modal Box added

--- src/Package.js
+++ src/Package.js
@@ -1,5 +1,31 @@
 import About from "./About.png"
+import { useState } from "react";
+
 const Package = () => {
+const [buyMtn, setBuyMtn] = useState(false)
+console.log(buyMtn)
+const handleMtnBuy = () =>{
+    setBuyMtn(true)
+}
+    const dataMtnPackage = 
+    [
+        {
+            name: "1g",
+            amount: 200,
+        },
+        {
+            name: "2g",
+            amount: 350,
+        },
+        {
+            name: "3g",
+            amount: 450,
+        },
+        
+        
+    ]
+       
+    
     return (  
             <>
                 <section>
@@ -14,12 +40,35 @@ const Package = () => {
                                             <div className="py-4 shadow-lg  border-2 border-blue-400 text-center rounded-lg w-40 mx-6 font-bold">MTN</div>
                                             
                                         <div className="bg-white mt-4 p-10 w-52 text-center shadow-lg rounded-lg">
-                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
-                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
-                                        <p className="text-blue-500 font-bold ">N300 = 1 gig</p>
+                                            {
+                                                dataMtnPackage.map((item, index) =>{
+                                                        return(
+                                                            <div key={index}>
+                                                             <p className="text-blue-500 font-bold ">{item.name} = N{item.amount}</p>
+                                      
+                                                            </div>
+                                                        )
+                                                })
+                                            }
+                                       
 
-                                            <button className="bg-blue-500 py-2 px-6 rounded-lg mt-4 font-bold text-white">Buy Now</button>
+                                            <button className="bg-blue-500 py-2 px-6 rounded-lg mt-4 font-bold text-white" onClick={handleMtnBuy}>Buy Now</button>
+                                            <div className="flex justify-center">
+                                                    <div className=" justify-center rounded-lg bg-white mtnModal">
+                                                        <div className="ml-auto">
+                                                            <span>x</span>
+                                                        </div>
+                                                            <p className="text-blue-500">Custom offer</p>
+                                                            <p>MTN</p>
+                                                            <input type = "text" placeholder= "Amount" className="border-2 border-blue-500 rounded-lg mx-4 text-center"/>
+                                                            <p>Total Cost = N300</p>
+
+                                                            <button className="bg-blue-500 text-white text-center">Make Payment</button>
+                                                    </div>
+                                            </div>
+                                            <div id="overlay" class="overlay"></div>
                                         </div>
+
                                         </div>
 
 
